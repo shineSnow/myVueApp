a853693e2cbb87b1718d06f28d7933cc41ef61ce
qiuming <qiu_ming@hunliji.com>
2018-03-06 14:09:53 +0800

    搭建vue项目结构，配置express作为项目服务器

---
+++ devServer.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+let port = 8080
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
