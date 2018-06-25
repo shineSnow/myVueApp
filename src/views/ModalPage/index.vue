0425d501edff5f9bb7357bd98d74d6b31524c979
shineSnow <1358205616@qq.com>
2018-06-25 15:30:13 +0800

    Modal

--- src/views/ModalPage/index.vue
+++ src/views/ModalPage/index.vue
@@ -1,20 +1,35 @@
 <template>
-  <div class="wrapper">
+    <div class="wrapper">
+        <Button  class="btn" @click="showModal">点我出点弹框</Button>
 
-    <Modal>
-      
-    </Modal>
+        <Modal v-show="isModalVisible" @close="closeModal">
 
-  </div>
+        </Modal>
+
+    </div>
 </template>
 <script>
-import Modal from 'CMP/Modal/index.vue'
-  export default {
-    name: 'ModalPage',
-    components:{
-      Modal,
+    import Modal from 'CMP/Modal/index.vue'
+
+    export default {
+        name: 'ModalPage',
+        components: {
+            Modal,
+        },
+        data() {
+            return {
+                isModalVisible:false,
+            }
+        },
+        methods:{
+            showModal() {
+                this.isModalVisible = true;
+            },
+            closeModal() {
+                this.isModalVisible = false;
+            }
+        }
     }
-  }
 </script>
 <style scoped>
 
